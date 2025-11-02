c0b94868c5f6867c3a90db7b9a0bc86dc22c233d
Dean-Sussman <sussmandean@icloud.com>
2025-11-02 00:00:56 -0400

    takes string for before and after dates, and uses defer/followup to aviod timeout messages

--- scraper_bot.py
+++ scraper_bot.py
@@ -15,25 +15,26 @@ async def on_ready():
 
 #scrapes all replay files from the specified channel(s)
 @bot.slash_command(name="scrape", description="Scrape for links after a given date")
-@option("year", description="Scrape messages sent starting from this year", required=True, min_value=2000, max_value=datetime.datetime.now().year)
-@option("month", description="Scrape messages sent starting from this month", required=True, min_value=1, max_value=12)
-@option("day", description="Scrape messages sent starting from this day", required=True, min_value=1, max_value=31)
+@option("afterdate", description="Scrape messages sent after this date (YYYY-MM-DD)", required=True)
+@option("beforedate", description="Scrape messages sent before this date (YYYY-MM-DD)", required=True)
 @option("channel ID", description="ID of the channel you want to scrape for replays", required=True)
 @option("second channel", required=False, default=None)
 @option("third channel", required=False, default=None)
 @option("fourth channel", required=False, default=None)
 @option("fifth channel", required=False, default=None)
 async def scrape(ctx: discord.ApplicationContext,
-                 year: int,
-                 month: int,
-                 day: int,
+                 beforedate: str,
+                 afterdate: str,
                  channel_id: str,
                  second_channel: str = None,
                  third_channel: str = None,
-                 fourth_channel: str = None):
+                 fourth_channel: str = None,
+                 fifth_channel: str = None):
+    await ctx.defer(ephemeral=True)
     # scrape links from the given start date until today; could prompt for end date as well, to avoid postseason
-    season_start = datetime.datetime(year, month, day)
-    channel_ids = [channel_id, second_channel, third_channel, fourth_channel]
+    season_end = datetime.datetime.strptime(beforedate, "%Y-%m-%d")
+    season_start = datetime.datetime.strptime(afterdate, "%Y-%m-%d")
+    channel_ids = [channel_id, second_channel, third_channel, fourth_channel, fifth_channel]
     channels = []
     # have to take IDs as strings and convert to ints after because of length
     # then, convert those strings into channels usable by the bot
@@ -44,14 +45,15 @@ async def scrape(ctx: discord.ApplicationContext,
     # iterate through each channel, checking messages from the given date for replay links
     # turn them into .json files and download them
     for channel in channels:
-        async for msg in channel.history(after=season_start):
+        async for msg in channel.history(before=season_end,after=season_start):
             if 'https://replay.pokemonshowdown' in msg.content:
                 links = re.findall(r'(https?://\S+)', msg.content)
                 for link in links:
+                    link = link.replace("?p2", "") # links ending in ?p2 break the .json convention
                     file_path = 'replay_' + str(i) + '.json'
                     i = i + 1
                     link = link + ".json"
                     urllib.request.urlretrieve(link, file_path)
-    await ctx.respond("Links scraped", ephemeral=True)
+    await ctx.followup.send("Links scraped", ephemeral=True)
 
 bot.run(os.getenv('TOKEN')) # run the bot with the token
